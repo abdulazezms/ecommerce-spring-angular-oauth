fb6d9deb5d3cdcec1ff30709c2275418e66244b9
Abdulazezms <abdulaziizmohammeds@gmail.com>
2023-07-11 01:10:59 +0300

    add cross origin support to allow JS code running on the host and port specified to access resources served by ProductCategoryDao

--- src/main/java/com/aziz/ecommerce/dao/ProductCategoryDao.java
+++ src/main/java/com/aziz/ecommerce/dao/ProductCategoryDao.java
@@ -3,6 +3,9 @@ package com.aziz.ecommerce.dao;
 import com.aziz.ecommerce.domain.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryDao extends JpaRepository<ProductCategory, Long> {}
