0f43d101649c235f82d703851cb3afb650893727
Abdulazezms <abdulaziizmohammeds@gmail.com>
2023-07-11 00:38:06 +0300

    add cross origin support to allow JS code running on the host and port specified

--- src/main/java/com/aziz/ecommerce/dao/ProductDao.java
+++ src/main/java/com/aziz/ecommerce/dao/ProductDao.java
@@ -2,5 +2,7 @@ package com.aziz.ecommerce.dao;
 
 import com.aziz.ecommerce.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductDao extends JpaRepository<Product, Long> {}
