63e3b22daeffb237392e9c7a113b66b58155447b
Abdulazezms <abdulaziizmohammeds@gmail.com>
2023-07-11 23:04:47 +0300

    expose entities IDs

--- src/main/java/com/aziz/ecommerce/config/RestConfiguration.java
+++ src/main/java/com/aziz/ecommerce/config/RestConfiguration.java
@@ -2,14 +2,26 @@ package com.aziz.ecommerce.config;
 
 import com.aziz.ecommerce.domain.Product;
 import com.aziz.ecommerce.domain.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import jakarta.persistence.metamodel.Type;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class RestConfiguration implements RepositoryRestConfigurer {
+    private EntityManager entityManager;
+    @Autowired
+    public RestConfiguration(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -25,6 +37,21 @@ public class RestConfiguration implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metadata, httpMethods) -> httpMethods.disable(unsupportedHttpMethods)))
                 .withCollectionExposure(((metadata, httpMethods) -> httpMethods.disable(unsupportedHttpMethods)));
+        exposeIds(config);
+
+    }
+
+    /**
+     * Expose entities' IDs.
+     * @param config REST configuration.
+     */
+    private void exposeIds(RepositoryRestConfiguration config) {
+        //get entity classes.
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        //get java type of each of them.
+        List<? extends Class<?>> entityClasses = entities.stream().map(Type::getJavaType).toList();
+        //expose the ID of each entity.
+        config.exposeIdsFor(entityClasses.toArray(new Class[0]));
 
     }
 }
--- src/main/java/com/aziz/ecommerce/dao/ProductDao.java
+++ src/main/java/com/aziz/ecommerce/dao/ProductDao.java
@@ -1,8 +1,15 @@
 package com.aziz.ecommerce.dao;
 
 import com.aziz.ecommerce.domain.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+
 @CrossOrigin("http://localhost:4200")
-public interface ProductDao extends JpaRepository<Product, Long> {}
+public interface ProductDao extends JpaRepository<Product, Long> {
+    //exposes endpoint /search/<query-method-name>
+    Page<Product> findByProductCategoryId(@Param("id") Long id, Pageable pageable);
+}
