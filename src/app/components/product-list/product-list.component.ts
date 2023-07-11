1c1ce0cbd0086e58ba067673e7f478d68dba679b
Abdulazezms <abdulaziizmohammeds@gmail.com>
2023-07-11 20:31:39 +0300

    implement product listing feature

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -4,7 +4,8 @@ import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  templateUrl: './product-list-grid.component.html',
+  // templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
